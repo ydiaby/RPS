c5daf7eeed0c8b17e1570bd184edc3206ca2c2fe
ydiaby <ydiaby21@gmail.com>
2022-05-06 19:44:57 -0400

    finished the functions

--- rps.js
+++ rps.js
@@ -18,27 +18,31 @@ return("You lose.")
 function game() {
 let pScore = 0;
 let comScore = 0;
-
-
+let tie = 0;
 for (let i = 0; i < 5; i++) {
+  let com = computerPlay() 
   let answer = prompt("Please choose Rock, Paper, or Scissors.").toLowerCase();;
-  if (playRound(answer, computerPlay) === "This is a tie"){
+  if (playRound(answer, com) === "This is a tie"){
     console.log("Tie Round")
+    tie = tie + 1
     
-  }else if (playRound(answer, computerPlay) === "You win"){
+    
+  }else if (playRound(answer, com) === "You win."){
     console.log("You won this round")
     pScore = pScore + 1 
     
-  }else if (playRound(answer, computerPlay) === "You lose") {
+  }else if (playRound(answer, com) === "You lose.") {
     console.log("You lost this round ")
     comScore = comScore + 1
     }
   }
-console.log(`You won ${pScore} rounds while the computer won ${pScore} rounds`)
+if (pScore > comScore){
+  console.log (`You won the game. You won ${pScore} out of five rounds.`)
+} else if (pScore === comScore){
+  console.log(`You tied the game. You won ${pScore} out of five rounds.`)
+}else{
+  console.log(`You lost the game. You won ${pScore} out of five rounds.`)
+}
 }
 
 game()
-
-
-
-
