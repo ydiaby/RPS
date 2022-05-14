8408ec4ec051eb417e22fa3bac5e550e597bd0ab
ydiaby <ydiaby21@gmail.com>
2022-05-14 18:36:47 -0400

    Finished barebones ui

--- rps.js
+++ rps.js
@@ -22,12 +22,10 @@ function game() {
 
   let pScore = 0;
   let comScore = 0;
+  let tie = 0;
   let rounds = 0; 
 
   const results = document.getElementById("results");
-  
-
- 
   const eachRound = document.createElement('div');
   eachRound.textContent = (`Rounds:  ${rounds}`);
   document.body.append(eachRound);
@@ -38,44 +36,47 @@ function game() {
   buttons.addEventListener('click', doSomething, false);
   
   function doSomething(e){
+    let com = computerPlay();  
     if (e.target !== e.currentTarget){
       let choice =  e.target.id.toString();
-      let com = computerPlay(); 
       if (playRound(choice, com) === "This is a tie"){
-        results.textContent = 'Tie Round';  
+        tie += 1;
+        results.textContent = 'Tie Round.';  
         rounds +=1;
         eachRound.textContent = `Rounds:  ${rounds}`;
-        
-
-
-        
       }else if (playRound(choice, com) === "You win."){ 
         pScore += 1;
-        results.textContent = 'You won this round';  
+        results.textContent = 'You won this round.';  
         rounds +=1;
         eachRound.textContent = `Rounds:  ${rounds}`;
-        
       }else if (playRound(choice, com) === "You lose.") {
         comScore += 1;
-        results.textContent = 'You lost this round';  
+        results.textContent = 'You lost this round.';  
         rounds +=1;
         eachRound.textContent = `Rounds:  ${rounds}`;
         }
     e.stopPropagation();
     }  
     if (rounds === 5 && pScore > comScore){
-      results.textContent = `You won the game. You won ${pScore} out of five rounds.`;
-      let rounds = 0;
+      results.textContent = `You won the game. You won ${pScore} rounds, tied ${tie} rounds, and lost ${comScore} rounds.`;
+      rounds = 0;
+      comScore = 0;
+      pScore = 0;
+      tie =0;
     }else if (rounds === 5 && pScore === comScore){
-      results.textContent = `You tied the game. You won ${pScore} out of five rounds.`; 
-      let rounds = 0;
-    }else if (rounds === 5 && pScore > comScore){
-      results.textContent = `You lost the game. You won ${pScore} out of five rounds.`;
-      let rounds = 0;
+      results.textContent = `You tied the game. You won ${pScore} rounds, tied ${tie} rounds, and lost ${comScore} rounds.`; 
+      rounds = 0;
+      comScore = 0;
+      pScore = 0;
+      tie =0;
+    }else if (rounds === 5 && pScore < comScore){
+      results.textContent = `You lost the game. You won ${pScore} rounds, tied ${tie} rounds, and lost ${comScore} rounds.`;
+      rounds = 0;
+      comScore = 0;
+      pScore = 0;
+      tie =0;
     } 
   }
-
-  
 
   } 
 /* Determines who won the round and adds it under the results div  */
