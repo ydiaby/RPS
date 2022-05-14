2e3268d3204ddede87f03822f1f47dc4195a3e94
ydiaby <ydiaby21@gmail.com>
2022-05-14 16:14:29 -0400

    Added Round Count and displayed winner

--- rps.js
+++ rps.js
@@ -19,52 +19,65 @@ return("You lose.")
 
 
 function game() {
-let pScore = 0;
-let comScore = 0;
-let tie = 0;
 
-const buttons = document.querySelector('#buttons');
-buttons.addEventListener('click', doSomething, false);
+  let pScore = 0;
+  let comScore = 0;
+  let rounds = 0; 
 
-function doSomething(e){
-  if (e.target !== e.currentTarget){
-    return e.target.id;
+  const results = document.getElementById("results");
+  
+
+ 
+  const eachRound = document.createElement('div');
+  eachRound.textContent = (`Rounds:  ${rounds}`);
+  document.body.append(eachRound);
+  
+
+
+  const buttons = document.querySelector('#buttons');
+  buttons.addEventListener('click', doSomething, false);
+  
+  function doSomething(e){
+    if (e.target !== e.currentTarget){
+      let choice =  e.target.id.toString();
+      let com = computerPlay(); 
+      if (playRound(choice, com) === "This is a tie"){
+        results.textContent = 'Tie Round';  
+        rounds +=1;
+        eachRound.textContent = `Rounds:  ${rounds}`;
+        
+
+
+        
+      }else if (playRound(choice, com) === "You win."){ 
+        pScore += 1;
+        results.textContent = 'You won this round';  
+        rounds +=1;
+        eachRound.textContent = `Rounds:  ${rounds}`;
+        
+      }else if (playRound(choice, com) === "You lose.") {
+        comScore += 1;
+        results.textContent = 'You lost this round';  
+        rounds +=1;
+        eachRound.textContent = `Rounds:  ${rounds}`;
+        }
+    e.stopPropagation();
+    }  
+    if (rounds === 5 && pScore > comScore){
+      results.textContent = `You won the game. You won ${pScore} out of five rounds.`;
+      let rounds = 0;
+    }else if (rounds === 5 && pScore === comScore){
+      results.textContent = `You tied the game. You won ${pScore} out of five rounds.`; 
+      let rounds = 0;
+    }else if (rounds === 5 && pScore > comScore){
+      results.textContent = `You lost the game. You won ${pScore} out of five rounds.`;
+      let rounds = 0;
+    } 
   }
-  e.stopPropagation();
-}
 
-/* Determines what button was clicked and returns that value  */
+  
 
-
-
-                
-for (let i = 0; i < 5; i++) {
-  let com = computerPlay();
-  let choice = doSomething;
-  if (playRound(choice, com) === "This is a tie"){
-    results.textContent = 'Tie Round';  
-    tie += 1;
-    
-    
-  }else if (playRound(choice, com) === "You win."){
-    results.textContent = "You won this round"; 
-    pScore += 1;
-    
-  }else if (playRound(choice, com) === "You lose.") {
-    results.textContent = 'You lost this round';
-    comScore += 1;
-    }
-  }
-  /* Plays 5 round and determines who won each round  */
-
-if (pScore > comScore){
-  results.textContent = `You won the game. You won ${pScore} out of five rounds.`;
-} else if (pScore === comScore){
-  results.textContent = `You tied the game. You won ${pScore} out of five rounds.`; 
-}else{
-  results.textContent = `You lost the game. You won ${pScore} out of five rounds.`;     
-}
-}
+  } 
 /* Determines who won the round and adds it under the results div  */
 
 game()
